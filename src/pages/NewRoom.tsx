5282cfe0b05f068e525bb4479ea8be8793d91822
Janderson Pizzially Vilas Boas <jandersonpizziallyvilasboas@gmail.com>
2021-06-30 21:05:34 -0300

    first commit

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,33 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+/**NewRoom Styles */
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie sala de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Crie uma nova sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>Quer entrar em uma sala já existente? <a href="#">Clique aqui</a></p>
+        </div>
+      </main>
+    </div>
+  )
+}
